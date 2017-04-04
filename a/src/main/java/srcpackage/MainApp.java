c8702f519b51aaad17a6484573bcade556b72d8d
Nicolae2095 <ciobanu.nicolae95@mail.ru>
2017-04-04 11:30:13 +0300

    Update MainApp.java

--- a/src/main/java/srcpackage/MainApp.java
+++ a/src/main/java/srcpackage/MainApp.java
@@ -6,7 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
 		Student student = (Student) context.getBean("student");
 		System.out.println("Name : " + student.getName());
 		System.out.println("Age : " + student.getAge());
